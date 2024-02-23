95694a0ec0dd3ea329a6b5d5aa8bc0c06489f981
IanWen103026 <114296116+IanWen103026@users.noreply.github.com>
2024-02-23 08:33:24 +0800

    a01

---
+++ A01.cpp
@@ -0,0 +1,6 @@
+#include<iostream> // 引入函式庫
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(0); cout.tie(0); // 這行不要看，I/O優化，對不起我在耍毒
+    cout<<"HelloWorld!\nHelloWorld!\nHelloWorld!"; // 輸出題目要求的
+}
